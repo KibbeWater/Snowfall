2ff3da477410d52bc946c22ed017735ce2b882ee
N1ceL <41979846+N1ceL@users.noreply.github.com>
2021-11-02 01:03:09 +1000

    02.11.2021

---
+++ CrackGame/GameSDK/Other.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+
+float AntiCheat::DecryptFloat(CodeStage_AntiCheat_ObscuredTypes_ObscuredFloat_o* pThis, const MethodInfo* pMethod)
+{
+	static auto fnDecryptFloat = reinterpret_cast<float(__thiscall*)(CodeStage_AntiCheat_ObscuredTypes_ObscuredFloat_o * pThis, const MethodInfo * pMethod)>(
+		MEM::PatternScan("GameAssembly.dll", "33 D2 E9 ? ? ? ? CC CC CC CC CC CC CC CC CC 48 89 5C 24 ? 57 48 83 EC 20 80 79 0C 00 48 8B FA 48 8B D9 75 28"));
+
+	return fnDecryptFloat(pThis, pMethod);
+}
+
--- CrackGame/GameSDK/Other.h
+++ CrackGame/GameSDK/Other.h
@@ -1,3 +1,6 @@
 #pragma once
 
-typedef float(__stdcall DecryptFloatFn)(CodeStage_AntiCheat_ObscuredTypes_ObscuredFloat_o* pThis, const MethodInfo* pMethod);
+namespace AntiCheat
+{
+	float DecryptFloat(CodeStage_AntiCheat_ObscuredTypes_ObscuredFloat_o* pThis, const MethodInfo* pMethod);
+}
